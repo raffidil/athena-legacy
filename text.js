e31285e1c5c0d7dc3d0d5e00a76ba75f9017fda8
Raffi Dilanchian <raffidilanchian@gmail.com>
2019-01-25 17:53:29 +0330

    Update text.js
    Former-commit-id: 6805310aa0f1e03f43dccfd80d78e9412ecbf366
    Former-commit-id: bbe6c3082578acf0db5e2b2b5ad59d48e6db46c3
    Former-commit-id: 7f0958af37795399aa04fd612c0d04c76e3cd42d

---
+++ text.js
@@ -0,0 +1,19 @@
+const text = {
+  title: 'Title',
+  homeTitle: 'Matches',
+  start: 'Start',
+  categories: 'Categories',
+  correct: 'Correct',
+  wrong: 'Wrong',
+  score: 'Score',
+  close: 'Close',
+  timeout: 'Time Out',
+  science: 'Science',
+  sciences: 'Sciences',
+  music: 'Music',
+  musics: 'Musics',
+
+
+};
+
+export default text;
