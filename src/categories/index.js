d80f0a16ebd06555f7a1565e2fff632924381277
Raffi Dilanchian <raffidilanchian@gmail.com>
2019-01-25 17:55:14 +0330

    Refactor categories page
    Former-commit-id: b4e4040289e2726420523c3a59be0c7707b0993a
    Former-commit-id: 8472655820ef1d28fde712ffa415beb8f124089c
    Former-commit-id: a2d49136cf2b56637956e97391ec222c98ce0993

--- src/categories/index.js
+++ src/categories/index.js
@@ -9,143 +9,73 @@ import {
   IconButton,
   Grid,
   Cell,
-  Icon
+  Icon,
 } from 'react-mdl';
 import Layout from '../../components/Layout';
 import s from './styles.css';
 import sciPic from './sci.png';
-import sportPic from './sport.jpg';
-import contactPic from './contact.png';
 import musicPic from './music.jpeg';
-import physicalPic from './physical.jpeg';
-import Link from '../../components/Link';
 import history from '../history';
+import text from '../../text';
+import categoryCard from '../../components/CategoryCard/CategoryCard';
+import CategoryCard from '../../components/CategoryCard/CategoryCard';
 
 
 class AboutPage extends React.Component {
 
   componentDidMount() {
-    document.title = 'Մրցոյթներ';
-  }
-
-  gotoHome() {
-    history.push({pathname: '/'}); // go to page function
-  }
-  gotoScience() {
-    history.push({pathname: '/science'}); // go to page function
-  }
-  gotoSport() {
-    history.push({pathname: '/sport'}); // go to page function
-  }
-  gotoImages() {
-    history.push({pathname: '/images'}); // go to page function
-  }
-  gotoMusics() {
-    history.push({pathname: '/musics'}); // go to page function
-  }
-  gotoPhysical() {
-    history.push({pathname: '/physical'}); // go to page function
+    document.title = text.categories;
   }
 
   render() {
-    const CardWidth = '400px';
-    const CardHeight = '140px';
+    const CardWidth = '300px';
+    const Categories = [
+      {
+        text: text.science,
+        image: sciPic,
+        path: '/science',
+      },
+      {
+        text: text.music,
+        image: musicPic,
+        path: '/musics',
+      },
+    ];
+
     return (
       <Layout className={s.content}>
-        <h1 className={s.fontstyle1} style={{
-        marginTop: '20px'
-      }}>
+        <h1
+          className={s.fontstyle1} style={{
+            marginTop: '20px',
+          }}
+        >
           <Grid className="demo-grid-1" >
-          <Cell col={1}>
-            <IconButton style={{color: '#310035'}} name="arrow_back" colored onClick={this.gotoHome}/>
-          </Cell>
-          <Cell col={1} style={{color: '#310035'}}>Մրցոյթներ</Cell>
-            </Grid>
-       </h1>
-        <div style={{
-          width: '100%',
-          margin: 'auto'
-        }}>
-          <Grid className="demo-grid-1">
-            <Cell col={6}>
-              <Card shadow={5} style={{
-                width: CardWidth,
-                height: '120px',
-                margin: 'auto'
-              }}>
-                <CardTitle style={{
-                  color: '#000',
-                  height: '176px',
-                  background: `url(${sciPic}) center / cover`,
-                }}></CardTitle>
-                <CardActions border>
-                  <Button colored style={{
-                    fontSize: 30,
-                    color: '#310035'
-                  }} onClick={this.gotoScience}>Փուլ 1</Button>
-                </CardActions>
-              </Card>
+            <Cell col={1}>
+              <IconButton style={{ color: '#263238' }} name="arrow_back" colored onClick={this.gotoHome} />
             </Cell>
-            <Cell col={6}>
-              <Card shadow={5} style={{
-                width: CardWidth,
-                height: '120px',
-                margin: 'auto'
-              }}>
-                <CardTitle style={{
-                  color: '#fff',
-                  height: '176px',
-                  background: `url(${musicPic}) center / cover`
-                }}></CardTitle>
-                <CardActions border>
-                  <Button colored style={{
-                    fontSize: 30,
-                    color: '#310035'
-                  }} onClick={this.gotoMusics}>Փուլ 2</Button>
-                </CardActions>
-              </Card>
-            </Cell>
+            <Cell col={1} style={{ color: '#263238', width: '80%' }}>{text.categories}</Cell>
           </Grid>
-          <Grid className="demo-grid-1">
-            <Cell col={6}>
-              <Card shadow={5} style={{
-                width: CardWidth,
-                height: '120px',
-                margin: 'auto'
-              }}>
-                <CardTitle style={{
-                  color: '#fff',
-                  height: '176px',
-                  background: `url(${contactPic}) center / cover`
-                }}></CardTitle>
-                <CardActions border>
-                  <Button colored style={{
-                    fontSize: 30,
-                    color: '#310035'
-                  }} onClick={this.gotoImages}>Փուլ 3</Button>
-                </CardActions>
-              </Card>
-            </Cell>
-            <Cell col={6}>
-              <Card shadow={5} style={{
-                width: CardWidth,
-                height: '120px',
-                margin: 'auto'
-              }}>
-                <CardTitle style={{
-                  color: '#fff',
-                  height: '176px',
-                  background: `url(${physicalPic}) center / cover`
-                }}></CardTitle>
-                <CardActions border>
-                  <Button colored style={{
-                    fontSize: 30,
-                    color: '#310035'
-                  }} onClick={this.gotoPhysical}>Փուլ 4</Button>
-                </CardActions>
-              </Card>
-            </Cell>
-          </Grid>
+        </h1>
+        <div
+          style={{
+            width: '100%',
+            margin: 'auto',
+            display: 'flex',
+            marginTop: '100px',
+          }}
+        >
+          {Categories.map(category =>
+            <CategoryCard
+              image={category.image}
+              onClick={() => { history.push({ pathname: category.path }); }}
+              width={CardWidth}
+              text={category.text}
+              height="300px"
+              cardHeight="250px"
+            />,
+          )}
+
+
         </div>
       </Layout>
     );
