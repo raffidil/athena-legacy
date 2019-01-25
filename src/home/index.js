8d2cbd652b7e5f5f2cc4c164f4cb52371e50dd02
Raffi Dilanchian <raffidilanchian@gmail.com>
2019-01-25 17:55:55 +0330

    Refactor home page
    Former-commit-id: 8462bcdfb0809e91da12d614c63c0f07f0a2e710
    Former-commit-id: 66e97a7613ce6d4b58ffebb6b33dae196e01a9ca
    Former-commit-id: 1a27d858c7d1b50c1a55655c60a7fcdd31ad1512

--- src/home/index.js
+++ src/home/index.js
@@ -1,5 +1,5 @@
   import React from 'react';
-import {
+  import {
   Button,
   CardText,
   Card,
@@ -15,69 +15,52 @@ import {
   DialogContent,
   DialogActions,
 } from 'react-mdl';
-import db from 'localforage';
-import Layout from '../../components/Layout';
-import s from './styles.css';
-import homepic from './l.png';
-import Link from '../../components/Link';
-import history from '../history';
+  import db from 'localforage';
+  import Layout from '../../components/Layout';
+  import s from './styles.css';
+  import homepic from './home.svg';
+  import Link from '../../components/Link';
+  import history from '../history';
+  import text from '../../text';
 
-class AboutPage extends React.Component {
+  class AboutPage extends React.Component {
 
-  componentDidMount() {
-    document.title = 'Home';
-  }
+    componentDidMount() {
+      document.title = 'Home';
+    }
 
-  gotoCategories() {
-    history.push({ pathname: '/categories' });
-  }
+    gotoCategories() {
+      history.push({ pathname: '/categories' });
+    }
 
-  playSong = () => {
-    const audio = new Audio(backSong);
-    audio.play();
-  }
+    playSong = () => {
+      const audio = new Audio(backSong);
+      audio.play();
+    }
 
-  render() {
-    return (
-      <Layout className={s.content}>
-        <div style={{
-          width: '100%',
-          margin: 'auto',
-          marginTop: '10px'
-        }}>
-                  <img src={homepic} style={{width: '30%',display: 'block',margin: 'auto',marginTop: '40px'}}/>
-                  <Button
-                    raised
-                    accent
-                    style={{ backgroundColor: '#821679',display: 'block',margin: 'auto',marginTop: '20px',height: 80, width:150 ,fontSize: 35}}
-                    onClick={this.gotoCategories}
-                    ripple>Start</Button>
-
-               {/*<Card shadow={5} style={{
-                width: '600px',
-                margin: 'auto',
-              }}>
-                <CardTitle style={{
-                  color: '#fff',
-                  height: '400px',
-                  background: `url(${homepic}) center / cover`,
-                  backgroundColor: '#a8629c'
-                }}/>
-                <CardActions border>
-                  <Button
-                    colored style={{
-                      fontSize: 20
-                    }} onClick={this.gotoCategories}
-                  >
-                  start
-                </Button>
-                </CardActions>
-              </Card>*/}
-        </div>
-      </Layout>
-    );
-  }
+    render() {
+      return (
+        <Layout className={s.content}>
+          <div
+            style={{
+              width: '100%',
+              margin: 'auto',
+              marginTop: '-10px',
+            }}
+          >
+            <img src={homepic} style={{ width: '55%', display: 'block', margin: 'auto', marginTop: '0px' }} />
+            <div style={{ fontSize: 30, flexDirection: 'column', textAlign: 'center', color: '#37474F', fontFamily: 'ArTarumianMatenagir' }}>{text.homeTitle}</div>
+            <Button
+              primary
+              style={{ color: '#263238', display: 'block', margin: 'auto', marginTop: '20px', height: 70, width: 200, fontSize: 35 }}
+              onClick={this.gotoCategories}
+              ripple
+            >{text.start}</Button>
+          </div>
+        </Layout>
+      );
+    }
 
 }
 
-export default AboutPage;
+  export default AboutPage;
